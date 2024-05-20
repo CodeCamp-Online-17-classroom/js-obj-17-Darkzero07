6aa5831d84d74986c91a42c39410b1af53bff6d7
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-20 22:45:43 +0700

    js lab 63

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// write code here
+const permission1 = { canView: false, canDelete: false };
+const permission2 = { canUpdate: true, canCreate: true };
+const permission3 = { canCreate: false, canDelete: true };
+const permission4 = { canView: true };
+
+// let permission = Object.assign({}, permission1, permission2, permission3, permission4);
+// console.log(permission);
+
+//or
+let permission = {...permission1, ...permission2, ...permission3, ...permission4};
+console.log(permission);
